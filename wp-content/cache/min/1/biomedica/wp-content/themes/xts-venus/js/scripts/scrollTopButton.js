8e155e33bcb4ef8ad44820fa58963478452d558b
Daim124-ux <sromek35@gmail.com>
2024-06-19 13:35:42 +0200

    test v04

--- wp-content/cache/min/1/biomedica/wp-content/themes/xts-venus/js/scripts/scrollTopButton.js
+++
@@ -1,2 +0,0 @@
-(function($){XTSThemeModule.scrollTopButton=function(){var $btn=$('.xts-scroll-to-top');if($btn.length<=0){return}
-XTSThemeModule.$window.on('scroll',function(){if($(this).scrollTop()>100){$btn.addClass('xts-shown')}else{$btn.removeClass('xts-shown')}});$btn.on('click',function(){$('html, body').animate({scrollTop:0},800);return!1})};$(document).ready(function(){XTSThemeModule.scrollTopButton()})})(jQuery)
